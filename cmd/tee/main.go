2900a981cf833f9270479a94d308d6803530388c
Josh Lubawy <jlubawy@awarepoint.com>
2017-02-09 13:49:34 -0800

    Add check for io.EOF. Change from Print to Println.

--- cmd/tee/main.go
+++ cmd/tee/main.go
@@ -3,26 +3,40 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 )
 
+var filenames []string
+var files []*os.File
+
 func main() {
-	filenames := os.Args[1:]
-	files := make([]*os.File, len(filenames))
+	filenames = os.Args[1:]
+	files = make([]*os.File, len(filenames))
+
 	for i, fname := range filenames {
 		f, err := os.OpenFile(fname, os.O_CREATE|os.O_APPEND, 0664)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err.Error())
 			os.Exit(1)
 		}
+		defer f.Close()
+
 		files[i] = f
 	}
 
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		fmt.Print(scanner.Text())
+		fmt.Println(scanner.Text())
 		for _, f := range files {
-			fmt.Fprint(f, scanner.Text())
+			fmt.Fprintln(f, scanner.Text())
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		if err != io.EOF {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
 		}
 	}
 }
